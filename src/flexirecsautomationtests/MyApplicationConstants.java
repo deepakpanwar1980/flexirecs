bbd315317d3e256aa3a2b9ec3efa2440888631e4
deepakpanwar1980 <deepakpanwar1980@yahoo.com>
2024-03-21 15:37:50 +0000

    first commit for flexirecs

---
+++ src/flexirecsautomationtests/MyApplicationConstants.java
@@ -0,0 +1,10 @@
+package flexirecsautomationtests;
+
+public class MyApplicationConstants 
+{
+	static String USER_NAME = "stafftester@flexirecs.com";
+	static String PASSOWRD = "Password2";
+	static String URL = "https://temp-staging.hiry.co.uk/login";
+	static String WRONG_USER_NAME = "deepakpanwar198@yahoo.com";
+	static String WRONG_PASSOWRD = "London123";
+}
